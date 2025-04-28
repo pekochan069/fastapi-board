64b45aafd875104c30955db82a293e095673c934
pekochan069 <pekochan069@gmail.com>
2025-04-28 17:30:37 +0900

    initial commit

---
+++ packages/ui/src/label.tsx
@@ -0,0 +1,19 @@
+import type { Component, ComponentProps } from "solid-js";
+import { splitProps } from "solid-js";
+import { cn } from "./cn";
+
+const Label: Component<ComponentProps<"label">> = (props) => {
+  const [local, others] = splitProps(props, ["class"]);
+  return (
+    // biome-ignore lint/a11y/noLabelWithoutControl: <explanation>
+    <label
+      class={cn(
+        "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+        local.class,
+      )}
+      {...others}
+    />
+  );
+};
+
+export { Label };
